7b072f636ffd062664441b303592682b1fed3703
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-14 00:24:50 +0400

    feat: Add user verify router

--- apps/backend/src/routes/customer/auth.ts
+++ apps/backend/src/routes/customer/auth.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "../../utils";
 import * as userAuthController from "../../controllers/customer/auth";
-import { userRegisterValidation } from "../../validations/customer";
+import {
+  userRegisterValidation,
+  userVerifyValidation,
+} from "../../validations/customer";
 import { validationResult } from "express-validator";
 
 export const userAuthRouter = Router();
@@ -17,6 +20,25 @@ userAuthRouter.post(
         return res.status(400).json({ errors: errors.array() });
       }
       return userAuthController.UserRegister(req, res, next);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+userAuthRouter.post(
+  "/verify",
+  userVerifyValidation,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      return userAuthController.UserVerify(req, res, next);
     } catch (error) {
       return res
         .status(500)
--- apps/backend/src/utils/errors/errors.ts
+++ apps/backend/src/utils/errors/errors.ts
@@ -26,9 +26,23 @@ export const errorMessages = {
     en: "Failed to send SMS.",
     ka: "SMS-ის გაგზავნა ვერ მოხერხდა.",
   },
+  verificationCodeExpired: {
+    en: "Verification code expired",
+    ka: "ვერიფიკაციის კოდი ვადა ამოეწურა",
+  },
+  smsCodeisInvalid: {
+    en: "SMS code is invalid",
+    ka: "SMS კოდი არ არის ვალიდური",
+  },
+
+  // Messages
   smsVerificationSent: {
     en: "Verification code sent to your phone number.",
     ka: "ვერიფიკაციის კოდი გამოიგზავნა თქვენს ტელეფონის ნომერზე.",
+  },
+  verificationSuccessful: {
+    en: "Verification successful",
+    ka: "ვერიფიკაცია წარმატებით დასრულდა",
   },
 
   // Validation errors
@@ -83,6 +97,14 @@ export const errorMessages = {
   passwordsNotMatch: {
     en: "Passwords must match",
     ka: "პაროლები არ ემთხვევა",
+  },
+  invalidId: {
+    en: "Invalid id",
+    ka: "id არასწორია",
+  },
+  invalidCode: {
+    en: "Invalid OTP code",
+    ka: "OTP კოდი არასწორია",
   },
 };
 
