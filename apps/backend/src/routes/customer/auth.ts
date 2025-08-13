32f6e2283774e35659e329a77b954370bc4a1c6a
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-14 01:29:46 +0400

    feat: Add user login router

--- apps/backend/src/routes/customer/auth.ts
+++ apps/backend/src/routes/customer/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "../../utils";
 import * as userAuthController from "../../controllers/customer/auth";
 import {
+  userLoginValidation,
   userRegisterValidation,
   userVerifyValidation,
 } from "../../validations/customer";
@@ -39,6 +40,25 @@ userAuthRouter.post(
         return res.status(400).json({ errors: errors.array() });
       }
       return userAuthController.UserVerify(req, res, next);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+userAuthRouter.post(
+  "/login",
+  userLoginValidation,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      return userAuthController.UserLogin(req, res, next);
     } catch (error) {
       return res
         .status(500)
