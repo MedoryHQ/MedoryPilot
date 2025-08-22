b90cdbce214cf47321c1a00b737b61874019d6b8
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-22 23:06:58 +0400

    feat: Configure vite of admin panel

--- apps/admin-panel/vite.config.ts
+++ apps/admin-panel/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+    nodePolyfills(),
+  ],
+  server: {
+    port: 4000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
