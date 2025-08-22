3ec8877ffb94ba46cbc5f25dd4ee56a7515369c7
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-22 23:16:46 +0400

    feat: Configure tailwind css

--- apps/admin-panel/vite.config.ts
+++ apps/admin-panel/vite.config.ts
@@ -2,20 +2,22 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "automatic",
+      jsxRuntime: "automatic"
     }),
     nodePolyfills(),
+    tailwindcss()
   ],
   server: {
-    port: 4000,
+    port: 4000
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
