b5af223792acdf9434b5950d8eefb2d13b15018b
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-23 21:56:29 +0400

    feat: Add login page routing

--- apps/admin-panel/src/Router.tsx
+++ apps/admin-panel/src/Router.tsx
@@ -1,25 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as Routes from "./routes";
 
-// const createRoutes = (routes: any) => {
-//   return Object.values(routes).map((route: any) => {
-//     if (!route?.path) {
-//       return route;
-//     }
+const createRoutes = (routes: any) => {
+  return Object.values(routes).map((route: any) => {
+    if (!route?.path) {
+      return route;
+    }
 
-//     return {
-//       ...route,
-//       path: `${route.path}`,
-//       element: <></>
-//     };
-//   });
-// };
+    return {
+      ...route,
+      path: `${route.path}`,
+      element: <></>
+    };
+  });
+};
 
 export const Router = () => {
   const routesWithLayout = [
     {
       path: "/",
       element: <>PraxisSync</>
-    }
+    },
+    ...createRoutes(Routes || "")
   ];
 
   const router = createBrowserRouter(routesWithLayout);
---
+++ apps/admin-panel/src/routes/authRoute.tsx
@@ -0,0 +1,7 @@
+import Login from "@/pages/Auth/Login";
+
+export const authNavigationRoute = {
+  element: <Login />,
+  path: "/auth",
+  isAuthRoute: true
+};
