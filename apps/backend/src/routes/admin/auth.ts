31deedfbfaa265ceb369c1388ed9a01bd8e69495
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-25 15:55:17 +0400

    feat: Add admin otp code resend route

--- apps/backend/src/routes/admin/auth.ts
+++ apps/backend/src/routes/admin/auth.ts
@@ -5,6 +5,7 @@ import {
   forgotAdminPasswordValidation,
   forgotAdminPasswordVerificationValidation,
   loginValidation,
+  resendValidation,
   resetAdminPasswordValidation,
   verifyValidation,
 } from "@/validations/admin/";
@@ -34,6 +35,20 @@ adminAuthRouter.post(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return authController.verifyOtp(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminAuthRouter.post(
+  "/resend-otp",
+  resendValidation,
+  validationHandler,
+  isAdminVerified,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return authController.resendAdminVerificationCode(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
