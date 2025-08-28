a1508c049adcfec857fec040826f24b0ab5bac82
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-08-28 23:38:54 +0400

    fix: Optimize authorized and unathorized admin  navigation flow

--- apps/admin-panel/src/pages/Auth/Login.tsx
+++ apps/admin-panel/src/pages/Auth/Login.tsx
@@ -17,6 +17,7 @@ const Login = () => {
 
   useEffect(() => {
     if (isLoggedIn) navigate("/");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoggedIn]);
 
   useEffect(() => {
