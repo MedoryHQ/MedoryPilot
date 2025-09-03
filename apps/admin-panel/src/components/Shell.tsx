4e90d47b4ec023050dca06e151baf447a681c796
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-03 23:06:38 +0400

    feat: Finish shell

--- apps/admin-panel/src/components/Shell.tsx
+++ apps/admin-panel/src/components/Shell.tsx
@@ -1,45 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Header } from "./Header";
+import { cn } from "@/libs";
+import { useSidebarStore } from "@/store";
 
 interface ShellProps {
   children: React.ReactNode;
 }
 
 export const Shell: React.FC<ShellProps> = ({ children }) => {
-  const [collapsed, setCollapsed] = useState(window.innerWidth < 768);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
+  const { collapsed, toggleCollapsed } = useSidebarStore();
   useEffect(() => {
     const handleResize = () => {
       const mobile = window.innerWidth < 768;
       setIsMobile(mobile);
 
       if (mobile && !collapsed) {
-        setCollapsed(true);
+        toggleCollapsed();
       }
     };
 
     handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [collapsed]);
 
   return (
-    <div className="site-frame">
+    <div className="bg-background min-h-screen">
       <Sidebar />
 
       <div
-        className="transition-all duration-200 ease-out"
-        style={{
-          marginLeft: isMobile ? "72px" : collapsed ? "72px" : "240px",
-          minHeight: "100vh"
-        }}
+        className={cn(
+          "bg-background min-h-screen transition-all duration-200 ease-out",
+          isMobile || collapsed ? "!ml-[72px]" : "ml-[240px]"
+        )}
       >
-        <div className="content-main-card">
-          <Header />
-          <main className="p-4 md:p-6 lg:p-8">{children}</main>
-        </div>
+        <Header />
+        <main className="p-4 md:p-6 lg:p-8">{children}</main>
       </div>
     </div>
   );
