3d7e82763e69b9f2c646c9b43852ad03cf2a91b0
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-03 23:11:22 +0400

    feat: Add query chaange in search

--- apps/admin-panel/src/components/ui/search.tsx
+++ apps/admin-panel/src/components/ui/search.tsx
@@ -1,16 +1,38 @@
 import { Search as SearchIcon } from "lucide-react";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Input } from "./input";
 import { toUpperCase } from "@/utils";
 import { useTranslation } from "react-i18next";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export const Search: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState("");
   const searchRef = useRef<HTMLDivElement>(null);
-  const handleSearch = (query: string) => {
-    setSearchQuery(query);
-  };
   const { t } = useTranslation();
+
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const [searchValue, setSearchValue] = useState<string>(
+    searchParams.get("search") || ""
+  );
+
+  const updateSearchParams = (key: string, value: string) => {
+    if (value) {
+      searchParams.set(key, value);
+    } else {
+      searchParams.delete(key);
+    }
+  };
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      updateSearchParams("search", searchValue);
+      navigate(`?${searchParams.toString()}`);
+    }, 500);
+
+    return () => clearTimeout(handler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchValue]);
 
   return (
     <div ref={searchRef} className="relative hidden sm:block">
@@ -19,8 +41,8 @@ export const Search: React.FC = () => {
       </div>
       <Input
         type="text"
-        value={searchQuery}
-        onChange={(e) => handleSearch(e.target.value)}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
         placeholder={toUpperCase(t("global.search"))}
         className="bg-muted/50 border-border h-10 w-48 rounded-lg pr-4 pl-12 transition-all duration-300 lg:w-96"
       />
