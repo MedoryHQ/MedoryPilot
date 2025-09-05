8d4c7d9f15220433207290f4783809f5f47b35de
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-05 21:56:44 +0400

    feat: Enhance color change from light to dark mode

--- apps/admin-panel/src/components/Sidebar/SidebarFooter.tsx
+++ apps/admin-panel/src/components/Sidebar/SidebarFooter.tsx
@@ -74,7 +74,7 @@ export const SidebarFooter: React.FC<SideBarFooterProps> = ({
               <DropdownMenuContent
                 side="top"
                 align="end"
-                className="w-56 rounded-xl border-0 bg-white px-3 shadow-xl"
+                className="w-56 rounded-xl border-0 bg-white px-3 text-black shadow-xl"
                 sideOffset={8}
               >
                 <DropdownMenuItem
