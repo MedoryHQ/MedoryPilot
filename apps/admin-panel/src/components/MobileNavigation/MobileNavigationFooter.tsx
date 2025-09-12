4348f772dd243cb9f40bd7225ebf46e3a870f34b
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-12 23:30:49 +0400

    feat: Add logout function into mobile navigation

--- apps/admin-panel/src/components/MobileNavigation/MobileNavigationFooter.tsx
+++ apps/admin-panel/src/components/MobileNavigation/MobileNavigationFooter.tsx
@@ -1,17 +1,17 @@
 import { useTranslation } from "react-i18next";
 import { Button } from "../ui";
 import { LogOut } from "lucide-react";
+import { useAuthStore } from "@/store";
 
 export const MobileNavigationFooter: React.FC = () => {
   const { t } = useTranslation();
+  const { logout } = useAuthStore();
   return (
     <div className="pb-8">
       <Button
         variant="outline"
         className="h-12 w-full justify-start gap-3 rounded-xl border-red-200 text-red-600 hover:border-red-300 hover:bg-red-50 hover:text-red-700"
-        onClick={() => {
-          window.location.reload();
-        }}
+        onClick={() => logout()}
       >
         <LogOut className="h-5 w-5" />
         <span className="font-medium">{t("sidebar.logout", "Logout")}</span>
