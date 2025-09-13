5f37800af456412ff9a7537a516ff44a326c0733
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-13 22:42:32 +0400

    feat: Add fetch header route for admin

--- apps/backend/src/routes/admin/website/header.ts
+++ apps/backend/src/routes/admin/website/header.ts
@@ -2,6 +2,8 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as headerController from "@/controllers/admin/website/header";
 import { isAdminVerified } from "@/middlewares/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
+import { fetchHeaderValidation } from "@/validations/admin";
 
 export const adminHeaderRouter = Router();
 
@@ -11,6 +13,20 @@ adminHeaderRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return headerController.fetchHeaders(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminHeaderRouter.get(
+  "/:id",
+  isAdminVerified,
+  fetchHeaderValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return headerController.fetchHeader(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
