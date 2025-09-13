82029aeed8b718a12fbc2619312ba34d4414126a
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-13 23:00:02 +0400

    feat: Add create header validation

--- apps/backend/src/validations/admin/website/header.validations.ts
+++ apps/backend/src/validations/admin/website/header.validations.ts
@@ -1,3 +1,4 @@
+import { validateTranslations } from "@/validations/shared";
 import { body } from "express-validator";
 
 export const fetchHeaderValidation = [
@@ -5,3 +6,21 @@ export const fetchHeaderValidation = [
 ];
 
 export const deleteHeaderValidation = fetchHeaderValidation;
+
+export const createHeaderValidation = [
+  body("active").optional().isBoolean().withMessage("invalidActiveType"),
+  body("logo")
+    .isObject()
+    .optional({ nullable: true })
+    .withMessage("invalidLogo"),
+  body("translations")
+    .isObject()
+    .custom((translations) =>
+      validateTranslations(translations, [
+        { name: "name", required: true },
+        { name: "position", required: true },
+        { name: "headline", required: true },
+        { name: "description", required: true },
+      ])
+    ),
+];
