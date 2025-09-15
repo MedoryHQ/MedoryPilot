57e736ae25e8340ce0d00a07216bbbd8b514ab71
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-15 22:49:00 +0400

    feat: Add fetch FAQ router

--- apps/backend/src/routes/admin/website/FAQ.ts
+++ apps/backend/src/routes/admin/website/FAQ.ts
@@ -2,6 +2,8 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as FAQController from "@/controllers/admin/website/FAQ";
 import { isAdminVerified } from "@/middlewares/admin";
+import { fetchFAQValidation } from "@/validations/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminFAQRouter = Router();
 
@@ -11,6 +13,20 @@ adminFAQRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return FAQController.fetchFAQs(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminFAQRouter.get(
+  "/:id",
+  isAdminVerified,
+  fetchFAQValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return FAQController.fetchFAQ(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
