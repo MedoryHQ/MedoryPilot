8b980a321c0e051b64a9eb89b07e029d63be6601
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-16 20:31:24 +0400

    feat: Add fetch service router

--- apps/backend/src/routes/admin/website/service.ts
+++ apps/backend/src/routes/admin/website/service.ts
@@ -2,6 +2,8 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as ServiceController from "@/controllers/admin/website/services";
 import { isAdminVerified } from "@/middlewares/admin";
+import { fetchServiceValidation } from "@/validations/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminServiceRouter = Router();
 
@@ -11,6 +13,20 @@ adminServiceRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return ServiceController.fetchServices(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminServiceRouter.get(
+  "/:id",
+  isAdminVerified,
+  fetchServiceValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return ServiceController.fetchService(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
