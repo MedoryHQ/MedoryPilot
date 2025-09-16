b7d605d2d88215899aabc1c714baa60ca0ee5b9f
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-16 22:06:05 +0400

    feat: Add create contact router

--- apps/backend/src/routes/admin/website/contact.ts
+++ apps/backend/src/routes/admin/website/contact.ts
@@ -2,6 +2,8 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as ContactController from "@/controllers/admin/website/contact";
 import { isAdminVerified } from "@/middlewares/admin";
+import { createContactValidation } from "@/validations/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminContactRouter = Router();
 
@@ -23,6 +25,20 @@ adminContactRouter.delete(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return ContactController.deleteContact(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminContactRouter.post(
+  "/",
+  isAdminVerified,
+  createContactValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return ContactController.createContact(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
