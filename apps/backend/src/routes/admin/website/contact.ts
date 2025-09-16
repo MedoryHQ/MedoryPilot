64ba86f91c3fe24eb2a81828f9e70bb2a8ec4099
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-16 22:04:53 +0400

    feat: Add delete contact router

--- apps/backend/src/routes/admin/website/contact.ts
+++ apps/backend/src/routes/admin/website/contact.ts
@@ -16,3 +16,15 @@ adminContactRouter.get(
     }
   }
 );
+
+adminContactRouter.delete(
+  "/",
+  isAdminVerified,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return ContactController.deleteContact(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
