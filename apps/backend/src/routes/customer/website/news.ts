196e01ff2da19255b217b0505fee677a09793b39
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-17 23:30:12 +0400

    feat: Add fetch news router for customer

--- apps/backend/src/routes/customer/website/news.ts
+++ apps/backend/src/routes/customer/website/news.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as newsController from "@/controllers/customer/website/news";
+import { fetchNewsValidation } from "@/validations/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const newsRouter = Router();
 
@@ -11,3 +13,16 @@ newsRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
   }
 });
+
+newsRouter.get(
+  "/:id",
+  fetchNewsValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return newsController.fetchNews(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
