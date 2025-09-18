af8477da60f43ca15683b753a643aa53ec4944e5
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-18 23:12:40 +0400

    feat: Add create blog controller

--- apps/backend/src/controllers/admin/website/blog.ts
+++ apps/backend/src/controllers/admin/website/blog.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/config";
 import {
+  createTranslations,
   generateWhereInput,
   getPaginationAndFilters,
   getResponseMessage,
@@ -12,6 +13,7 @@ import {
   logAdminWarn as logWarn,
 } from "@/utils";
 import { Prisma } from "@prisma/client";
+import { CreateBlogDTO } from "@/types/admin";
 
 export const fetchBlogs = async (
   req: Request,
@@ -180,6 +182,69 @@ export const deleteBlog = async (
       ip: (req as any).hashedIp,
       id: (req as any).userId,
       event: "admin_delete_blog_exception",
+    });
+    next(error);
+  }
+};
+
+export const createBlog = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { translations, background, categories, ...rest } =
+      req.body as CreateBlogDTO;
+
+    logInfo("Blog create attempt", {
+      ip: (req as any).hashedIp,
+      id: (req as any).userId,
+      path: req.path,
+      event: "blog_create_attempt",
+    });
+
+    const translationsToCreate = Prisma.validator<
+      Prisma.BlogTranslationCreateWithoutBlogInput[]
+    >()(createTranslations(translations) as any);
+
+    const backgroundToCreate = background
+      ? {
+          path: background.path,
+          name: background.name,
+          size: background.size,
+        }
+      : undefined;
+
+    const blog = await prisma.blog.create({
+      data: {
+        ...rest,
+        categories: {
+          connect: categories.map((id) => ({
+            id,
+          })),
+        },
+        translations: { create: translationsToCreate },
+        ...(backgroundToCreate
+          ? { background: { create: backgroundToCreate } }
+          : {}),
+      },
+    });
+
+    logInfo("Blog created successfully", {
+      ip: (req as any).hashedIp,
+      id: (req as any).userId,
+      path: req.path,
+      event: "blog_created",
+    });
+
+    return res.json({
+      data: blog,
+    });
+  } catch (error) {
+    logCatchyError("create_blogs_exception", error, {
+      ip: (req as any).hashedIp,
+      id: (req as any).userId,
+      event: "admin_create_blogs_exception",
     });
     next(error);
   }
