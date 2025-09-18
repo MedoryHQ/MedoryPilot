cbe35213e1e2489481109fd6e63b49ff0564f380
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-18 23:21:41 +0400

    feat: Add fetch blog router

--- apps/backend/src/routes/customer/website/blog.ts
+++ apps/backend/src/routes/customer/website/blog.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as blogController from "@/controllers/customer/website/blog";
+import { fetchBlogValidation } from "@/validations/customer";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const blogRouter = Router();
 
@@ -11,3 +13,16 @@ blogRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
   }
 });
+
+blogRouter.get(
+  "/:slug",
+  fetchBlogValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return blogController.fetchBlog(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
