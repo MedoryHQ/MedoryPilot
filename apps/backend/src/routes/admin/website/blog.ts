3b4b226b1f368ac72f7b49ab6b3b20664b807b49
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-18 23:09:47 +0400

    feat: Add create blog router

--- apps/backend/src/routes/admin/website/blog.ts
+++ apps/backend/src/routes/admin/website/blog.ts
@@ -2,7 +2,11 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as blogController from "@/controllers/admin/website/blog";
 import { isAdminVerified } from "@/middlewares/admin";
-import { deleteBlogValidation, fetchBlogValidation } from "@/validations/admin";
+import {
+  createBlogValidation,
+  deleteBlogValidation,
+  fetchBlogValidation,
+} from "@/validations/admin";
 import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminBlogRouter = Router();
@@ -41,6 +45,20 @@ adminBlogRouter.delete(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return blogController.deleteBlog(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminBlogRouter.post(
+  "/",
+  isAdminVerified,
+  createBlogValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return blogController.createBlog(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
