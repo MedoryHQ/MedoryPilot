6b1775b687b9a2e3163313bc1dce235d06a49bb0
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-19 22:19:21 +0400

    feat: Add fetch social controller

--- apps/backend/src/controllers/admin/website/social.ts
+++ apps/backend/src/controllers/admin/website/social.ts
@@ -1,5 +1,9 @@
 import { prisma } from "@/config";
-import { generateWhereInput, getPaginationAndFilters } from "@/utils";
+import {
+  generateWhereInput,
+  getPaginationAndFilters,
+  sendError,
+} from "@/utils";
 import { NextFunction, Response, Request } from "express";
 import {
   logAdminError as logCatchyError,
@@ -21,7 +25,6 @@ export const fetchSocials = async (
         skip,
         take,
         orderBy,
-
         include: {
           icon: true,
         },
@@ -35,6 +38,45 @@ export const fetchSocials = async (
       ip: (req as any).hashedIp,
       id: (req as any).userId,
       event: "admin_fetch_socials_exception",
+    });
+    next(error);
+  }
+};
+
+export const fetchSocial = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+
+    const social = await prisma.social.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        icon: true,
+      },
+    });
+
+    if (!social) {
+      logWarn("Social fetch failed: social not found", {
+        ip: (req as any).hashedIp,
+        id: (req as any).userId,
+        path: req.path,
+
+        event: "social_fetch_failed",
+      });
+      return sendError(req, res, 404, "socialNotFound");
+    }
+
+    return res.status(200).json({ data: social });
+  } catch (error) {
+    logCatchyError("fetch_social_exception", error, {
+      ip: (req as any).hashedIp,
+      id: (req as any).userId,
+      event: "admin_fetch_social_exception",
     });
     next(error);
   }
