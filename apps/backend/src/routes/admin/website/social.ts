725a11bbd6a17f0d2a908fbfbc74b9fd8eb993cc
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-19 22:20:10 +0400

    feat: Add delete social router

--- apps/backend/src/routes/admin/website/social.ts
+++ apps/backend/src/routes/admin/website/social.ts
@@ -2,7 +2,10 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as SocialController from "@/controllers/admin/website/social";
 import { isAdminVerified } from "@/middlewares/admin";
-import { fetchSocialValidation } from "@/validations/admin";
+import {
+  deleteSocialValidation,
+  fetchSocialValidation,
+} from "@/validations/admin";
 import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminSocialRouter = Router();
@@ -27,6 +30,20 @@ adminSocialRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return SocialController.fetchSocial(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminSocialRouter.delete(
+  "/:id",
+  isAdminVerified,
+  deleteSocialValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return SocialController.deleteSocial(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
