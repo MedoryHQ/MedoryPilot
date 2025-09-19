033c7aaf5e3534b361272aaa34d695b906a060ea
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-19 21:18:24 +0400

    feat: Add delete page component router

--- apps/backend/src/routes/admin/website/page-component.ts
+++ apps/backend/src/routes/admin/website/page-component.ts
@@ -2,7 +2,10 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as pageComponentController from "@/controllers/admin/website/page-component";
 import { isAdminVerified } from "@/middlewares/admin";
-import { fetchPageComponentValidation } from "@/validations/admin";
+import {
+  deletePageComponentValidation,
+  fetchPageComponentValidation,
+} from "@/validations/admin";
 import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminPageComponentRouter = Router();
@@ -27,6 +30,20 @@ adminPageComponentRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return pageComponentController.fetchPageComponent(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminPageComponentRouter.delete(
+  "/:slug",
+  isAdminVerified,
+  deletePageComponentValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return pageComponentController.deletePageComponent(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
