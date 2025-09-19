e9aa08223ce9b767aba176264917b39d518ded33
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-19 21:16:24 +0400

    feat: Add fetch page component route for admin

--- apps/backend/src/routes/admin/website/page-component.ts
+++ apps/backend/src/routes/admin/website/page-component.ts
@@ -2,6 +2,8 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as pageComponentController from "@/controllers/admin/website/page-component";
 import { isAdminVerified } from "@/middlewares/admin";
+import { fetchPageComponentValidation } from "@/validations/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminPageComponentRouter = Router();
 
@@ -11,6 +13,20 @@ adminPageComponentRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return pageComponentController.fetchPageComponents(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminPageComponentRouter.get(
+  "/:slug",
+  isAdminVerified,
+  fetchPageComponentValidation
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return pageComponentController.fetchPageComponent(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
