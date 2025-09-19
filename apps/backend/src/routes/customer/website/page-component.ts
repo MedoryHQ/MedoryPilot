30bf69fe4618d88bc54cef649eed57a006bed8fc
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-19 22:00:46 +0400

    feat: Add fetch page component router for customer

--- apps/backend/src/routes/customer/website/page-component.ts
+++ apps/backend/src/routes/customer/website/page-component.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
-import * as pageComponentController from "@/controllers/customer/website/pageComponent";
+import * as pageComponentController from "@/controllers/customer/website/page-component";
+import { fetchPageComponentValidation } from "@/validations/customer";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const pageComponentRouter = Router();
 
@@ -9,6 +11,19 @@ pageComponentRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return pageComponentController.fetchPageComponents(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+pageComponentRouter.get(
+  "/:slug",
+  fetchPageComponentValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return pageComponentController.fetchPageComponent(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
