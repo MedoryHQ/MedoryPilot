75591c8685d4bda0687e764cbf751ec40398f29e
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-19 22:19:05 +0400

    feat: Add fetch social router

--- apps/backend/src/routes/admin/website/social.ts
+++ apps/backend/src/routes/admin/website/social.ts
@@ -2,6 +2,8 @@ import { NextFunction, Router, Request, Response } from "express";
 import { GLOBAL_ERROR_MESSAGE } from "@/utils";
 import * as SocialController from "@/controllers/admin/website/social";
 import { isAdminVerified } from "@/middlewares/admin";
+import { fetchSocialValidation } from "@/validations/admin";
+import { validationHandler } from "@/middlewares/global/validationHandler";
 
 export const adminSocialRouter = Router();
 
@@ -11,6 +13,20 @@ adminSocialRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return SocialController.fetchSocials(req, res, next);
+    } catch {
+      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
+    }
+  }
+);
+
+adminSocialRouter.get(
+  "/:id",
+  isAdminVerified,
+  fetchSocialValidation,
+  validationHandler,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return SocialController.fetchSocial(req, res, next);
     } catch {
       res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
     }
