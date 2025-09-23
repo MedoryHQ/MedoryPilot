6fca8d8f4ea280bce063814f298b2284832f57c8
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-23 23:49:12 +0400

    fix: Remove unnecesery routes of header

--- apps/backend/src/controllers/admin/website/header.ts
+++ apps/backend/src/controllers/admin/website/header.ts
@@ -13,66 +13,13 @@ import {
 import { Prisma } from "@prisma/client";
 import { CreateHeaderDTO, UpdateHeaderDTO } from "@/types/admin";
 
-export const fetchHeaders = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { skip, take, orderBy, search } = getPaginationAndFilters(req);
-
-    const where = generateWhereInput<Prisma.HeaderWhereInput>(search, {
-      "translations.some.name": "insensitive",
-      "translations.some.position": "insensitive",
-      "translations.some.headline": "insensitive",
-      "translations.some.description": "insensitive",
-    });
-
-    const [headers, count] = await Promise.all([
-      prisma.header.findMany({
-        skip,
-        take,
-        orderBy,
-        where,
-        include: {
-          logo: true,
-          translations: {
-            include: {
-              language: {
-                select: {
-                  code: true,
-                },
-              },
-            },
-          },
-        },
-      }),
-      prisma.header.count({ where }),
-    ]);
-
-    return res.status(200).json({ data: headers, count });
-  } catch (error) {
-    logCatchyError("fetch_headers_exception", error, {
-      ip: (req as any).hashedIp,
-      id: (req as any).userId,
-      event: "admin_fetch_headers_exception",
-    });
-    next(error);
-  }
-};
-
 export const fetchHeader = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { id } = req.params;
-
-    const header = await prisma.header.findUnique({
-      where: {
-        id,
-      },
+    const header = await prisma.header.findFirst({
       include: {
         logo: true,
         translations: {
--- apps/backend/src/routes/admin/website/header.ts
+++ apps/backend/src/routes/admin/website/header.ts
@@ -6,7 +6,6 @@ import { validationHandler } from "@/middlewares/global/validationHandler";
 import {
   createHeaderValidation,
   deleteHeaderValidation,
-  fetchHeaderValidation,
   updateHeaderValidation,
 } from "@/validations/admin";
 
@@ -15,20 +14,6 @@ export const adminHeaderRouter = Router();
 adminHeaderRouter.get(
   "/",
   isAdminVerified,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      return headerController.fetchHeaders(req, res, next);
-    } catch {
-      res.status(500).json({ errors: [{ message: GLOBAL_ERROR_MESSAGE }] });
-    }
-  }
-);
-
-adminHeaderRouter.get(
-  "/:id",
-  isAdminVerified,
-  fetchHeaderValidation,
-  validationHandler,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       return headerController.fetchHeader(req, res, next);
