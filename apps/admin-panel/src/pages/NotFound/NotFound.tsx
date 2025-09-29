edae21ca78bb380a88824bb2fa69e2818c8dce04
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-09-29 23:48:28 +0400

    feat: Enhance not found page

--- apps/admin-panel/src/pages/NotFound/NotFound.tsx
+++ apps/admin-panel/src/pages/NotFound/NotFound.tsx
@@ -1,24 +1,32 @@
-import { Button, Result } from "antd";
-import { useNavigate } from "react-router-dom";
+import { Icons } from "@/components";
+import { toUpperCase } from "@/utils";
+import { Button } from "antd";
+import { useTranslation } from "react-i18next";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export const NotFound = ({ subTitle }: { subTitle?: string }) => {
+export const NotFound = () => {
   const navigate = useNavigate();
-
+  const location = useLocation();
+  const { t } = useTranslation();
   const navigateBack = () => {
     navigate(-1);
   };
 
   return (
-    <Result
-      className="w-full"
-      status="404"
-      title="404"
-      subTitle={subTitle || ""}
-      extra={
+    <div className="flex h-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-[24px] pb-[56px]">
+        <Icons.NotFound className="h-[152px] w-[152px]" />
+        <h2 className="text-[20px] md:text-[26px]">
+          {toUpperCase(t("notFound.pageNotFound"))}
+        </h2>
+        <p className="text-center">
+          {toUpperCase(t("notFound.resourceOfGiven"))}: {location.pathname}{" "}
+          {toUpperCase(t("notFound.cannotBeFound"))}
+        </p>
         <Button type="primary" onClick={navigateBack}>
-          This page not exist
+          {toUpperCase(t("notFound.navigateBack"))}
         </Button>
-      }
-    />
+      </div>
+    </div>
   );
 };
