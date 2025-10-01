0010bd9fc3cfb21465b6c30372bf104042d860de
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-01 22:51:29 +0400

    feat: Add get header query

--- apps/admin-panel/src/libs/queries/headers.ts
+++ apps/admin-panel/src/libs/queries/headers.ts
@@ -1,6 +1,11 @@
-import { HeadersResponse } from "@/types/website";
+import {
+  HeaderFormValues,
+  HeaderResponse,
+  HeadersResponse
+} from "@/types/website";
 import instance from "../../api/axios";
 import { useQuery } from "react-query";
+import { UseFormSetValue } from "react-hook-form";
 
 export const useGetHeaders = (search?: URLSearchParams) => {
   return useQuery<HeadersResponse, Error>({
@@ -12,5 +17,52 @@ export const useGetHeaders = (search?: URLSearchParams) => {
       return data;
     },
     refetchOnWindowFocus: false
+  });
+};
+
+export const useGetHeader = (
+  id: string | null,
+  setValue: UseFormSetValue<HeaderFormValues>
+) => {
+  return useQuery<HeaderResponse, Error>({
+    queryKey: ["headers"],
+    queryFn: async (): Promise<HeaderResponse> => {
+      const { data } = await instance.get<HeaderResponse>(`/header/${id}`);
+      return data;
+    },
+    enabled: !!id,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess(data: HeaderResponse) {
+      const { translations, active, logo } = data.data;
+
+      const enTranslation = translations?.find(
+        (translation) => translation.language.code === "en"
+      );
+      const kaTranslation = translations?.find(
+        (translation) => translation.language.code === "ka"
+      );
+
+      const formTranslations = {
+        en: {
+          description: enTranslation?.description || "",
+          headline: enTranslation?.headline || "",
+          position: enTranslation?.position || "",
+          name: enTranslation?.name || ""
+        },
+        ka: {
+          name: kaTranslation?.name || "",
+          position: kaTranslation?.position || "",
+          description: kaTranslation?.description || "",
+          headline: kaTranslation?.headline || ""
+        }
+      };
+
+      setValue("active", active);
+      setValue("logo", logo);
+      setValue("translations", formTranslations);
+    }
   });
 };
