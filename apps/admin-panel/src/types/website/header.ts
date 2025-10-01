f5b82d53cee75eea3dbd1dcd970815984d80c88e
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-01 22:37:37 +0400

    feat: Add header query types

---
+++ apps/admin-panel/src/types/website/header.ts
@@ -0,0 +1,33 @@
+import { File, Language } from "../global";
+
+export interface HeadersResponse {
+  data: Header[];
+  count: number;
+}
+
+export interface HeaderResponse {
+  data: Header;
+}
+
+export interface Header {
+  id: string;
+  logo: File | null;
+  active: boolean | null;
+  translations: HeaderTranslation[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface HeaderTranslation {
+  id: string;
+  name: string;
+  position: string;
+  headline: string;
+  description: string;
+  header: Header;
+  headerId: string;
+  language: Language;
+  languageId: string;
+  createdAt: string;
+  updatedAt: string;
+}
