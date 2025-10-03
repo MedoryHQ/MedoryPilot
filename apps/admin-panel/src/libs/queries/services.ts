de5b04991598b9160bfc4cabc7309726eb90e1df
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-03 22:41:57 +0400

    feat: Add get service query

--- apps/admin-panel/src/libs/queries/services.ts
+++ apps/admin-panel/src/libs/queries/services.ts
@@ -19,3 +19,46 @@ export const useGetServices = (search?: URLSearchParams) => {
     refetchOnWindowFocus: false
   });
 };
+
+export const useGetService = (
+  id: string | null,
+  setValue: UseFormSetValue<ServiceFormValues>
+) => {
+  return useQuery<ServiceResponse, Error>({
+    queryKey: ["services"],
+    queryFn: async (): Promise<ServiceResponse> => {
+      const { data } = await instance.get<ServiceResponse>(`/service/${id}`);
+      return data;
+    },
+    enabled: !!id,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess(data: ServiceResponse) {
+      const { translations, background, icon } = data.data;
+
+      const enTranslation = translations?.find(
+        (translation) => translation.language.code === "en"
+      );
+      const kaTranslation = translations?.find(
+        (translation) => translation.language.code === "ka"
+      );
+
+      const formTranslations = {
+        en: {
+          description: enTranslation?.description || "",
+          title: enTranslation?.title || ""
+        },
+        ka: {
+          title: kaTranslation?.title || "",
+          description: kaTranslation?.description || ""
+        }
+      };
+
+      setValue("background", background);
+      setValue("icon", icon);
+      setValue("translations", formTranslations);
+    }
+  });
+};
