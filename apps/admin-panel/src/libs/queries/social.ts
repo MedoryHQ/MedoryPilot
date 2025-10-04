795b28de16bb47776af39cbd6728830932a0993d
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-04 23:51:10 +0400

    feat: Add get social query

--- apps/admin-panel/src/libs/queries/social.ts
+++ apps/admin-panel/src/libs/queries/social.ts
@@ -19,3 +19,28 @@ export const useGetSocials = (search?: URLSearchParams) => {
     refetchOnWindowFocus: false
   });
 };
+
+export const useGetSocial = (
+  id: string | null,
+  setValue: UseFormSetValue<SocialFormValues>
+) => {
+  return useQuery<SocialResponse, Error>({
+    queryKey: ["socials"],
+    queryFn: async (): Promise<SocialResponse> => {
+      const { data } = await instance.get<SocialResponse>(`/social/${id}`);
+      return data;
+    },
+    enabled: !!id,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess(data: SocialResponse) {
+      const { icon, name, url } = data.data;
+
+      setValue("icon", icon);
+      setValue("name", name);
+      setValue("url", url);
+    }
+  });
+};
