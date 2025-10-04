4c57abf81dfe1553a835a1e2bf349e09b1f4da00
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-04 22:17:17 +0400

    feat: Add introduce types

--- apps/admin-panel/src/types/website/index.ts
+++ apps/admin-panel/src/types/website/index.ts
@@ -1,3 +1,4 @@
 export * from "./overview";
 export * from "./header";
 export * from "./services";
+export * from "./introduce";
---
+++ apps/admin-panel/src/types/website/introduce.ts
@@ -0,0 +1,42 @@
+import { Language } from "../global";
+
+export interface IntroducesResponse {
+  data: Introduce[];
+  count: number;
+}
+
+export interface IntroduceResponse {
+  data: Introduce;
+}
+
+export interface Introduce {
+  id: string;
+  translations: IntroduceTranslation[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IntroduceTranslation {
+  id: string;
+  headline: string;
+  description: string;
+  introduce: Introduce;
+  introduceId: string;
+  language: Language;
+  languageId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type IntroduceFormValues = {
+  translations: {
+    en: {
+      headline: string;
+      description: string;
+    };
+    ka: {
+      headline: string;
+      description: string;
+    };
+  };
+};
