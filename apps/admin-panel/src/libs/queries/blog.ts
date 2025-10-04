0e2d013e62b606574b6c7ebbf10e8163198d6385
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-04 23:47:09 +0400

    feat: Add get blog query

--- apps/admin-panel/src/libs/queries/blog.ts
+++ apps/admin-panel/src/libs/queries/blog.ts
@@ -15,3 +15,65 @@ export const useGetBlogs = (search?: URLSearchParams) => {
     refetchOnWindowFocus: false
   });
 };
+
+export const useGetBlog = (
+  slug: string | null,
+  setValue: UseFormSetValue<BlogFormValues>
+) => {
+  return useQuery<BlogResponse, Error>({
+    queryKey: ["blogs"],
+    queryFn: async (): Promise<BlogResponse> => {
+      const { data } = await instance.get<BlogResponse>(`/blog/${slug}`);
+      return data;
+    },
+    enabled: !!slug,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess(data: BlogResponse) {
+      const {
+        translations,
+        background,
+        slug,
+        showInLanding,
+        categories,
+        landingOrder,
+        metaDescription,
+        metaImage,
+        metaKeywords,
+        metaTitle
+      } = data.data;
+
+      const enTranslation = translations?.find(
+        (translation) => translation.language.code === "en"
+      );
+      const kaTranslation = translations?.find(
+        (translation) => translation.language.code === "ka"
+      );
+
+      const formTranslations = {
+        en: {
+          content: enTranslation?.content || "",
+          title: enTranslation?.title || ""
+        },
+        ka: {
+          content: kaTranslation?.content || "",
+          title: kaTranslation?.title || ""
+        }
+      };
+
+      const filteredCategories = categories?.map((category) => category.id);
+      setValue("categories", filteredCategories);
+      setValue("slug", slug);
+      setValue("landingOrder", landingOrder);
+      setValue("showInLanding", showInLanding);
+      setValue("metaDescription", metaDescription || "");
+      setValue("metaImage", metaImage || null);
+      setValue("metaKeywords", metaKeywords || "");
+      setValue("metaTitle", metaTitle || "");
+      setValue("background", background);
+      setValue("translations", formTranslations);
+    }
+  });
+};
