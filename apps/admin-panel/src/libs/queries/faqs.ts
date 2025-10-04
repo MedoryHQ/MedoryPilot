9069a784aa6bd5d725e33f4c4dce471f7c33d95a
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-04 23:28:22 +0400

    feat: Add get FAQ query

--- apps/admin-panel/src/libs/queries/faqs.ts
+++ apps/admin-panel/src/libs/queries/faqs.ts
@@ -15,3 +15,45 @@ export const useGetFaqs = (search?: URLSearchParams) => {
     refetchOnWindowFocus: false
   });
 };
+
+export const useGetFaq = (
+  id: string | null,
+  setValue: UseFormSetValue<FaqFormValues>
+) => {
+  return useQuery<FaqResponse, Error>({
+    queryKey: ["faqs"],
+    queryFn: async (): Promise<FaqResponse> => {
+      const { data } = await instance.get<FaqResponse>(`/faq/${id}`);
+      return data;
+    },
+    enabled: !!id,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess(data: FaqResponse) {
+      const { translations, order } = data.data;
+
+      const enTranslation = translations?.find(
+        (translation) => translation.language.code === "en"
+      );
+      const kaTranslation = translations?.find(
+        (translation) => translation.language.code === "ka"
+      );
+
+      const formTranslations = {
+        en: {
+          question: enTranslation?.question || "",
+          answer: enTranslation?.answer || ""
+        },
+        ka: {
+          question: kaTranslation?.question || "",
+          answer: kaTranslation?.answer || ""
+        }
+      };
+
+      setValue("order", order);
+      setValue("translations", formTranslations);
+    }
+  });
+};
