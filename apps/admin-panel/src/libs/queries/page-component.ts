f2496cb320756e45c420d95e007550f254ff7c3e
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-04 23:49:36 +0400

    feat: Add get page component query

--- apps/admin-panel/src/libs/queries/page-component.ts
+++ apps/admin-panel/src/libs/queries/page-component.ts
@@ -19,3 +19,60 @@ export const useGetPageComponents = (search?: URLSearchParams) => {
     refetchOnWindowFocus: false
   });
 };
+
+export const useGetPageComponent = (
+  slug: string | null,
+  setValue: UseFormSetValue<PageComponentFormValues>
+) => {
+  return useQuery<PageComponentResponse, Error>({
+    queryKey: ["pageComponents"],
+    queryFn: async (): Promise<PageComponentResponse> => {
+      const { data } = await instance.get<PageComponentResponse>(
+        `/pageComponent/${slug}`
+      );
+      return data;
+    },
+    enabled: !!slug,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess(data: PageComponentResponse) {
+      const {
+        translations,
+        footerOrder,
+        slug,
+        metaDescription,
+        metaImage,
+        metaKeywords,
+        metaTitle
+      } = data.data;
+
+      const enTranslation = translations?.find(
+        (translation) => translation.language.code === "en"
+      );
+      const kaTranslation = translations?.find(
+        (translation) => translation.language.code === "ka"
+      );
+
+      const formTranslations = {
+        en: {
+          content: enTranslation?.content || "",
+          name: enTranslation?.name || ""
+        },
+        ka: {
+          content: kaTranslation?.content || "",
+          name: kaTranslation?.name || ""
+        }
+      };
+
+      setValue("slug", slug);
+      setValue("metaDescription", metaDescription || "");
+      setValue("metaImage", metaImage || null);
+      setValue("metaKeywords", metaKeywords || "");
+      setValue("metaTitle", metaTitle || "");
+      setValue("footerOrder", footerOrder || 0);
+      setValue("translations", formTranslations);
+    }
+  });
+};
