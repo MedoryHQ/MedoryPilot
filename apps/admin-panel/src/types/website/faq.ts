c4c473aa23a44c00d5c8fba9efc028e4de23ea09
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-04 23:13:44 +0400

    feat: Add FAQ types

---
+++ apps/admin-panel/src/types/website/faq.ts
@@ -0,0 +1,44 @@
+import { Language } from "../global";
+
+export interface FaqsResponse {
+  data: Faq[];
+  count: number;
+}
+
+export interface FaqResponse {
+  data: Faq;
+}
+
+export interface Faq {
+  id: string;
+  order: number;
+  translations: FaqTranslation[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface FaqTranslation {
+  id: string;
+  question: string;
+  answer: string;
+  faq: Faq;
+  faqId: string;
+  language: Language;
+  languageId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type FaqFormValues = {
+  order: number;
+  translations: {
+    en: {
+      question: string;
+      answer: string;
+    };
+    ka: {
+      question: string;
+      answer: string;
+    };
+  };
+};
