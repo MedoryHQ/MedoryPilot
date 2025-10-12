b23928943e693b1a119a15b10449e21fd953b4fc
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-12 22:47:17 +0400

    fix: Optimize contact query

--- apps/admin-panel/src/libs/queries/contact.ts
+++ apps/admin-panel/src/libs/queries/contact.ts
@@ -1,47 +1,17 @@
-import { ContactFormValues, ContactResponse } from "@/types/website";
+import { ContactResponse } from "@/types/website";
 import instance from "../../api/axios";
 import { useQuery } from "react-query";
-import { UseFormSetValue } from "react-hook-form";
 
-export const useGetContact = (
-  id: string | null,
-  setValue: UseFormSetValue<ContactFormValues>
-) => {
+export const useGetContact = () => {
   return useQuery<ContactResponse, Error>({
-    queryKey: ["contacts"],
+    queryKey: ["contact"],
     queryFn: async (): Promise<ContactResponse> => {
-      const { data } = await instance.get<ContactResponse>(`/contact/${id}`);
+      const { data } = await instance.get<ContactResponse>("/contact");
       return data;
     },
-    enabled: !!id,
-    refetchOnReconnect: false,
-    refetchInterval: false,
-    refetchOnMount: false,
+    refetchOnMount: true,
     refetchOnWindowFocus: false,
-    onSuccess(data: ContactResponse) {
-      const { translations, background, location } = data.data;
-
-      const enTranslation = translations?.find(
-        (translation) => translation.language.code === "en"
-      );
-      const kaTranslation = translations?.find(
-        (translation) => translation.language.code === "ka"
-      );
-
-      const formTranslations = {
-        en: {
-          title: enTranslation?.title || "",
-          description: enTranslation?.description || ""
-        },
-        ka: {
-          title: kaTranslation?.title || "",
-          description: kaTranslation?.description || ""
-        }
-      };
-
-      setValue("location", location);
-      setValue("background", background);
-      setValue("translations", formTranslations);
-    }
+    refetchOnReconnect: false,
+    refetchInterval: false
   });
 };
