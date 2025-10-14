ca019a6d32b02dd197ab51d6bd2418d428fbf51a
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-14 23:53:05 +0400

    fix: Optimize page components queries

--- apps/admin-panel/src/libs/queries/page-component.ts
+++ apps/admin-panel/src/libs/queries/page-component.ts
@@ -1,11 +1,6 @@
-import {
-  PageComponentFormValues,
-  PageComponentResponse,
-  PageComponentsResponse
-} from "@/types/website";
+import { PageComponentResponse, PageComponentsResponse } from "@/types/website";
 import instance from "../../api/axios";
 import { useQuery } from "react-query";
-import { UseFormSetValue } from "react-hook-form";
 
 export const useGetPageComponents = (search?: URLSearchParams) => {
   return useQuery<PageComponentsResponse, Error>({
@@ -20,12 +15,9 @@ export const useGetPageComponents = (search?: URLSearchParams) => {
   });
 };
 
-export const useGetPageComponent = (
-  slug: string | null,
-  setValue: UseFormSetValue<PageComponentFormValues>
-) => {
+export const useGetPageComponent = (slug: string | null) => {
   return useQuery<PageComponentResponse, Error>({
-    queryKey: ["pageComponents"],
+    queryKey: ["pageComponents", slug],
     queryFn: async (): Promise<PageComponentResponse> => {
       const { data } = await instance.get<PageComponentResponse>(
         `/pageComponent/${slug}`
@@ -33,46 +25,9 @@ export const useGetPageComponent = (
       return data;
     },
     enabled: !!slug,
-    refetchOnReconnect: false,
-    refetchInterval: false,
-    refetchOnMount: false,
+    refetchOnMount: true,
     refetchOnWindowFocus: false,
-    onSuccess(data: PageComponentResponse) {
-      const {
-        translations,
-        footerOrder,
-        slug,
-        metaDescription,
-        metaImage,
-        metaKeywords,
-        metaTitle
-      } = data.data;
-
-      const enTranslation = translations?.find(
-        (translation) => translation.language.code === "en"
-      );
-      const kaTranslation = translations?.find(
-        (translation) => translation.language.code === "ka"
-      );
-
-      const formTranslations = {
-        en: {
-          content: enTranslation?.content || "",
-          name: enTranslation?.name || ""
-        },
-        ka: {
-          content: kaTranslation?.content || "",
-          name: kaTranslation?.name || ""
-        }
-      };
-
-      setValue("slug", slug);
-      setValue("metaDescription", metaDescription || "");
-      setValue("metaImage", metaImage || null);
-      setValue("metaKeywords", metaKeywords || "");
-      setValue("metaTitle", metaTitle || "");
-      setValue("footerOrder", footerOrder || 0);
-      setValue("translations", formTranslations);
-    }
+    refetchOnReconnect: false,
+    refetchInterval: false
   });
 };
