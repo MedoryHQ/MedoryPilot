4f6a639ad4883d425c4adcf377a8f47e2023b3cc
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-25 23:58:50 +0400

    feat(frontend): Add files initialization

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  /* any other config options you have */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+      },
+      {
+        protocol: (process.env.NEXT_PUBLIC_PROTOCOL as "https") || "http",
+        hostname: process.env.NEXT_PUBLIC_HOSTNAME || "localhost",
+        pathname: "/uploads/**/*",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
---
+++ apps/frontend/src/hooks/index.ts
@@ -0,0 +1,10 @@
+export {
+  useCarouselFadeEdge,
+  getFadeEdgeVariants,
+  fadeEdgeVariants,
+} from "./use-carousel-fade-edge";
+export { useAuthMutation } from "./use-auth-mutation";
+export { useCartQuery } from "./use-cart-mutation";
+export { useIsMobile } from "./use-mobile";
+export { useCloseOnOutsideOrEsc } from "./use-close-on-outside-or-esc";
+export { useDebounce } from "./use-debounce";
