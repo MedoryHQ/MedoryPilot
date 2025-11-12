0e618120cfe0d8a50b6067f34c4bf0ffcf1cf64c
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-11-12 23:24:51 +0400

    feat(frontend): Add hero skeleton

--- apps/frontend/src/components/ui/skeleton.tsx
+++ apps/frontend/src/components/ui/skeleton.tsx
@@ -1,15 +1,33 @@
+"use client";
 import { cn } from "@/utils";
+import { Skeleton as HeroUISkeleton } from "@heroui/react";
 
 function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+    <HeroUISkeleton
+      className={cn("rounded-md bg-gray-100", className)}
       {...props}
     />
   );
 }
 
-export { Skeleton };
+const HeroSkeletion = () => {
+  return (
+    <section
+      id="home"
+      className="relative pt-8 overflow-hidden mb-6 md:mb-8 lg:mb-10"
+    >
+      <div className="mx-auto">
+        <div className="grid lg:grid-cols-2 gap-6">
+          <Skeleton className="rounded-4xl p-8 h-[700px]" />
+          <Skeleton className="rounded-4xl p-8 h-[700px]" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { Skeleton, HeroSkeletion };
