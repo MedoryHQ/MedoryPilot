4f4ef171709c60e936a638b89f6c49d5f3f1d603
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-11-18 23:47:03 +0400

    feat(frontend): Implement introduce in home page

--- apps/frontend/src/app/[locale]/(main)/page.tsx
+++ apps/frontend/src/app/[locale]/(main)/page.tsx
@@ -1,9 +1,10 @@
-import { Hero, Services } from "@/components";
+import { Hero, Services, Introduce } from "@/components";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Introduce />
       <Services />
     </div>
   );
