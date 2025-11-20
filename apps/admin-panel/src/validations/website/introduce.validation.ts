d9d43ca43188b0cbc574c0bf894e37340c27cfd7
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-11-20 22:17:52 +0400

    fix(cms): Optimize introduce validation and types

--- apps/admin-panel/src/validations/website/introduce.validation.ts
+++ apps/admin-panel/src/validations/website/introduce.validation.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 import { TFunction } from "i18next";
+import { FileSchema } from "../global.validation";
 
-export const introduceSchema = (t: TFunction<"translation", undefined>) =>
+export const introduceSchema = (
+  t: TFunction<"translation", undefined>,
+  lang: "en" | "ka" = "en"
+) =>
   z.object({
     translations: z.object({
       en: z.object({
@@ -20,7 +24,19 @@ export const introduceSchema = (t: TFunction<"translation", undefined>) =>
           .string()
           .min(1, { message: t("introduce.errors.descriptionRequired", "ka") })
       })
-    })
+    }),
+    thumbnail: FileSchema.nullable().refine(
+      (file) => file === null || typeof file === "object",
+      {
+        message: t("introduce.errors.thumbnailRequired", lang)
+      }
+    ),
+    video: FileSchema.nullable().refine(
+      (file) => file === null || typeof file === "object",
+      {
+        message: t("introduce.errors.videoRequired", lang)
+      }
+    )
   });
 
 export type IntroduceFormValues = z.infer<ReturnType<typeof introduceSchema>>;
