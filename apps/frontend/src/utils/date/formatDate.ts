b732272d92150b82b0d2fdd088791b99666dd136
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-12-01 23:54:16 +0400

    feat(frontend): Add date utils

---
+++ apps/frontend/src/utils/date/formatDate.ts
@@ -0,0 +1,23 @@
+import { georgianMonths } from "@/lib/siteData";
+import dayjs from "dayjs";
+import "dayjs/locale/ka";
+
+dayjs.locale("en");
+
+export const formatDate = (
+  date: string | Date,
+  language: string,
+  renderTime?: boolean
+) => {
+  const day = dayjs(date).date();
+  const year = dayjs(date).year();
+
+  if (language === "ka") {
+    const month = georgianMonths[dayjs(date).month()];
+    return `${day} ${month} ${year} ${
+      renderTime ? dayjs(date).format("HH:mm") : ""
+    }`;
+  } else {
+    return dayjs(date).format(`DD MMMM YYYY ${renderTime ? "HH:mm" : ""}`);
+  }
+};
